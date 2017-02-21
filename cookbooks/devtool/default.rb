0c506c43c86510883e4791819f2577294c0d14c4
k-kurumi <optpia.kurumi@gmail.com>
2017-02-21 14:07:28 +0900

    remove neobundle

--- cookbooks/devtool/default.rb
+++ cookbooks/devtool/default.rb
@@ -163,14 +163,13 @@ execute "install vim" do
   not_if "test -e /usr/local/bin/vim"
 end
 
-execute "install neobundle" do
+execute "install vim-plug" do
   user node.user
   command <<-EOL
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   EOL
 
-  not_if "test -e ~/.vim/bundle/neobundle.vim"
+  not_if "test -e ~/.vim/autoload/plug.vim"
 end
 
 # cf
